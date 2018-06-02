d1f026eb135e0c65abcf981e9f79ee2284ca5709
osmanok <33768464+osmanok@users.noreply.github.com>
2018-06-02 18:03:07 +0300

    String changes

---
+++ Day-02/string.py
@@ -0,0 +1,9 @@
+string="hi world"
+
+print("uzunluk", len(string))
+print("title", string.title())
+print("upper", string.upper())
+print("lower", string.lower())
+print("isupper", string.isupper())
+print("starswith", string.startswith("start"))
+print("endswirh", string.endswith("end"))
